929acff0f609951b28de996e89bc80e477a7b7c2
Ql1on <vadimeray2006@gmail.com>
2023-07-04 05:38:30 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+"use strict";
+const inputPasswordEl = document.querySelector('input.passw');
+const inputReTypePasswordEl = document.querySelector('input.retype_passw');
+const formEl = document.querySelector('form');
+
+// inputReTypePasswordEl.style.border = '2px solid red';
+inputReTypePasswordEl.addEventListener('input', () => {
+    if (inputReTypePasswordEl.value === inputPasswordEl.value) {
+        inputReTypePasswordEl.style.border = '2px solid green'
+    }
+    else if (inputReTypePasswordEl.value !== inputPasswordEl.value) {
+        inputReTypePasswordEl.style.border = '2px solid red';
+    }
+
+});
+formEl.addEventListener('submit', (event) => {
+    if (inputReTypePasswordEl.value !== inputPasswordEl.value) {
+        event.preventDefault();
+        alert('пароли не совпадают!');
+    }
+    starsEls.forEach((starEl) => {
+        starEl.style.fill = '#FFD36F';
+    });
+
+
+
+});
+let rating = '';
+const starsEls = document.querySelectorAll('.star_item');
+starsEls.forEach((star) => {
+
+    star.addEventListener('click', () => {
+        starsEls.forEach((starEl) => {
+            starEl.style.fill = '#FFD36F';
+        });
+        for (let i = 0; i < star.id; i++) {
+
+            starsEls[i].style.fill = 'goldenrod';
+        }
+        rating = star.id;
+
+    });
+
+});
+
